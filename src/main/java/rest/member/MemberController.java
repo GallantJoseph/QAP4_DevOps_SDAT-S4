69a42140a539557990ced25e28097a6b1762912f
GallantJoseph <gallant.joseph@outlook.com>
2025-12-01 17:34:51 -0330

    Member basic CRUD implementation

--- src/main/java/rest/member/MemberController.java
+++ src/main/java/rest/member/MemberController.java
@@ -1,12 +1,38 @@
 package rest.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/members")
 @CrossOrigin
 public class MemberController {
     @Autowired
     private MemberService memberService;
+
+    @PostMapping()
+    public ResponseEntity<Member> saveMember(@RequestBody Member member) {
+        return new ResponseEntity<>(memberService.saveMember(member), HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Member>> getAllMembers() {
+        return new ResponseEntity<>(memberService.getAllMembers(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Member> updateMemberById(@PathVariable("id") long id, @RequestBody Member member) {
+        return new ResponseEntity<>(memberService.updateMemberById(id, member), HttpStatus.OK) ;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Member> deleteMemberById(@PathVariable("id") long id) {
+        return new ResponseEntity<>(memberService.deleteMemberById(id), HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/rest/member/MemberService.java
+++ src/main/java/rest/member/MemberService.java
@@ -3,8 +3,51 @@ package rest.member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MemberService {
     @Autowired
     private MemberRepository memberRepository;
+
+    public Member saveMember(Member member) {
+        return memberRepository.save(member);
+    }
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member updateMemberById(Long id, Member member) {
+        Optional<Member> memberOptional = memberRepository.findById(id);
+
+        if (memberOptional.isEmpty())
+            return null;
+
+        Member memberToUpdate = memberOptional.get();
+
+        memberToUpdate.setFirstName(member.getFirstName());
+        memberToUpdate.setLastName(member.getLastName());
+        memberToUpdate.setStreetAddress(member.getStreetAddress());
+        memberToUpdate.setCity(member.getCity());
+        memberToUpdate.setProvince(member.getProvince());
+        memberToUpdate.setEmail(member.getEmail());
+        memberToUpdate.setPhone(member.getPhone());
+        memberToUpdate.setStartDate(member.getStartDate());
+        memberToUpdate.setMembershipDuration(member.getMembershipDuration());
+
+        return memberRepository.save(memberToUpdate);
+    }
+
+    public Member deleteMemberById(Long id) {
+        Optional<Member> memberOptional = memberRepository.findById(id);
+
+        if (memberOptional.isEmpty())
+            return null;
+
+        memberRepository.deleteById(id);
+
+        return memberOptional.get();
+    }
 }
